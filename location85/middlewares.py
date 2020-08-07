2dd1a01f8a856364d55d558752e0a770fb23482b
zengboling <zengboling@gmail.com>
2020-08-06 17:00:53 -0700

    add scrapy-rotated-proxy
    use scrapy-rotated-proxy package for rotating proxies.
    add 1.2 seconds delay for each request.

--- location85/middlewares.py
+++ location85/middlewares.py
@@ -5,6 +5,7 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 import base64
+import time
 from urllib.parse import unquote, urlunparse
 from urllib.request import getproxies, proxy_bypass
 
@@ -84,6 +85,7 @@ class Location85DownloaderMiddleware:
         # - or return a Request object
         # - or raise IgnoreRequest: process_exception() methods of
         #   installed downloader middleware will be called
+        time.sleep(1.2)
         return None
 
     def process_response(self, request, response, spider):
@@ -108,64 +110,3 @@ class Location85DownloaderMiddleware:
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
 
-class HttpProxyMiddleware:
-
-    def __init__(self, auth_encoding='latin-1'):
-        self.auth_encoding = auth_encoding
-        self.proxies = {}
-        for type_, url in getproxies().items():
-            self.proxies[type_] = self._get_proxy(url, type_)
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        if not crawler.settings.getbool('HTTPPROXY_ENABLED'):
-            raise NotConfigured
-        auth_encoding = crawler.settings.get('HTTPPROXY_AUTH_ENCODING')
-        return cls(auth_encoding)
-
-    def _basic_auth_header(self, username, password):
-        user_pass = to_bytes(
-            '%s:%s' % (unquote(username), unquote(password)),
-            encoding=self.auth_encoding)
-        return base64.b64encode(user_pass)
-
-    def _get_proxy(self, url, orig_type):
-        proxy_type, user, password, hostport = _parse_proxy(url)
-        proxy_url = urlunparse((proxy_type or orig_type, hostport, '', '', '', ''))
-
-        if user:
-            creds = self._basic_auth_header(user, password)
-        else:
-            creds = None
-
-        return creds, proxy_url
-
-    def process_request(self, request, spider):
-        # ignore if proxy is already set
-        if 'proxy' in request.meta:
-            if request.meta['proxy'] is None:
-                return
-            # extract credentials if present
-            creds, proxy_url = self._get_proxy(request.meta['proxy'], '')
-            request.meta['proxy'] = proxy_url
-            if creds and not request.headers.get('Proxy-Authorization'):
-                request.headers['Proxy-Authorization'] = b'Basic ' + creds
-            return
-        elif not self.proxies:
-            return
-
-        parsed = urlparse_cached(request)
-        scheme = parsed.scheme
-
-        # 'no_proxy' is only supported by http schemes
-        if scheme in ('http', 'https') and proxy_bypass(parsed.hostname):
-            return
-
-        if scheme in self.proxies:
-            self._set_proxy(request, scheme)
-
-    def _set_proxy(self, request, scheme):
-        creds, proxy = self.proxies[scheme]
-        request.meta['proxy'] = proxy
-        if creds:
-            request.headers['Proxy-Authorization'] = b'Basic ' + creds
